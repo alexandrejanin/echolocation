24c70d14d8082759fa02588e4319482af3af90d3
Alexandre Janin <alexandrejanin92@gmail.com>
2019-05-08 23:19:44 +0200

    fix code pour nouveau sonar

--- Programmes/SonarVibrationHCSR04/SonarVibrationHCSR04.ino
+++ Programmes/SonarVibrationHCSR04/SonarVibrationHCSR04.ino
@@ -7,14 +7,14 @@
 
 // Distance a laquelle les vibrations sont maximales, en cm
 #define distMin 10
-// Distance a laquelle les vibrations sont minimales, en cm 
+// Distance a laquelle les vibrations sont minimales, en cm
 #define distMax 200
 // Delai de vibration minimal, en ms (0 = continu)
 #define delayMin 0
 // Delai de vibration maximal, en ms
 #define delayMax 2000
 
-// Temps ecoule depuis la derniere vibration (en ms) 
+// Temps ecoule depuis la derniere vibration (en ms)
 int currentDelay = 0;
 // Temps de la derniere execution de loop()
 int lastTime = 0;
@@ -25,32 +25,31 @@ void setup() {
   Serial.begin(9600);
   // vibrPin = pin de sortie
   pinMode(vibrPin, OUTPUT);
+  // trigPin = pin de sortie
+  pinMode(trigPin, OUTPUT);
+  // echoPin = pin d'entree
+  pinMode(echoPin, INPUT);
 }
 
 // Distance captee par le capteur en cm
-int sonarCm() {
-  pinMode(trigPin, OUTPUT);
+float sonarCm() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  // sonarPin = pin d'entree
-  pinMode(echoPin, INPUT);
   // Delai de retour d'ultrasons du capteur
   float pulse = pulseIn(echoPin, HIGH);
-  // Distance en pouces
-  float inches = pulse / 147; // 147 uS per inch
   // Distance en cm
-  float cm = inches * 2.54; // 2,54 cm per inch
+  float cm = pulse / 2 / 29.1; // duree -> cm (343 m/s)
 
-  return (int) cm;
+  return cm;
 }
 
 // Boucle principale du programme
 void loop() {
   // Recupere la distance mesuree par le capteur
-  int cm = sonarCm();
+  float cm = sonarCm();
   // Affiche la distance si l'Arduino est branche a un ordinateur
   Serial.print("distance: ");
   Serial.print(cm);
@@ -58,12 +57,12 @@ void loop() {
 
   // On limite la distance aux valeurs donnees au debut du fichier
   int constrainedCm = constrain(cm, distMin, distMax);
-  
+
   // Delai entre les bips selon la distance:
   // Si la distance est minimale, le delai est minimal
   // Si la distance est maximale, le delai est maximal
   int del = map(constrainedCm, distMin, distMax, delayMin, delayMax);
-  
+
   // On augmente currentDelay du nombre de millisecondes ecoulees
   // depuis la derniere loop()
   // afin de mesurer le temps ecoule depuis la derniere vibration
@@ -75,9 +74,10 @@ void loop() {
   analogWrite(vibrPin, 0);
   // Si le delai depuis la derniere vibration est superieur au delai
   // calcule d'apres la distance...
-  if (currentDelay >= del){
+  if (currentDelay >= del) {
     // ...on emet une vibration
     analogWrite(vibrPin, 153);
+    delay(100);
     // Et on remet le delai actuel a 0
     currentDelay = 0;
   }
